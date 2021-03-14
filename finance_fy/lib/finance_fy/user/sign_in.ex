0c23f7d1f5b3ebf23acf81b2710a80899cc20c90
Eduardo Baptista <eduardo.baptista@vulcanet.com.br>
2021-03-14 20:43:19 -0300

    🚀 - Add sign in user method

---
+++ finance_fy/lib/finance_fy/user/sign_in.ex
@@ -0,0 +1,20 @@
+defmodule FinanceFy.User.SignIn do
+@moduledoc """
+  Sign in user and generates authentication token
+"""
+  import FinanceFy.Guardian
+
+  alias FinanceFy.Repo
+  alias FinanceFy.User
+
+  def call(email, password) do
+    with %User{} = user <- Repo.get_by(User, email: email),
+         true <- Bcrypt.verify_pass(password, user.password_hash),
+         {:ok, token, _claims} <- encode_and_sign(user)
+    do
+      {:ok, user, token}
+    else
+      _ -> {:error, :authentication_fail}
+    end
+  end
+end
---
+++ finance_fy/test/finance_fy/user/sign_in_test.exs
@@ -0,0 +1,56 @@
+defmodule FinanceFy.User.SignInTest do
+  @moduledoc """
+    Sign in user and generates authentication token
+  """
+    use FinanceFy.DataCase, async: true
+
+    alias FinanceFy.Guardian
+    alias FinanceFy.User
+    alias FinanceFy.User.SignIn
+
+    describe "call/2" do
+      setup do
+        params = %{
+          email: "shinzou@sasageyo.com",
+          name: "Tatakae",
+          password: "abc@123",
+          password_confirmation: "abc@123"
+        }
+
+        {:ok, user} = Repo.insert(User.changeset(params))
+
+        %{created_user: user}
+      end
+
+      test "When provide a valid email and password it should return a valid token", %{created_user: user} do
+        # Act
+        {:ok, _user, token} = SignIn.call(user.email, user.password)
+        {:ok, claims} = Guardian.decode_and_verify(token)
+
+        # Assert
+        assert claims["sub"] === user.id
+      end
+
+      test "When provide a wrong email it should return authentication error", %{created_user: user} do
+        # Arrange
+        wrong_email = "wrong_email"
+
+        # Act
+        {:error, reason} = SignIn.call(wrong_email, user.password)
+
+        # Assert
+        assert reason === :authentication_fail
+      end
+
+      test "When provide a wrong password it should return authentication error", %{created_user: user} do
+        # Arrange
+        wrong_password = "wrong_password"
+
+        # Act
+        {:error, reason} = SignIn.call(user.email, wrong_password)
+
+        # Assert
+        assert reason === :authentication_fail
+      end
+    end
+  end
