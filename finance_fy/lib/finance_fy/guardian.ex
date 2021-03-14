3961842949088861da89d50b441ec114edd74301
Eduardo Baptista <eduardo.baptista@vulcanet.com.br>
2021-03-14 19:54:08 -0300

    🚀 - Add guardian module

---
+++ finance_fy/lib/finance_fy/guardian.ex
@@ -0,0 +1,20 @@
+defmodule FinanceFy.Guardian do
+@moduledoc """
+  Handle JWT authentication and generation
+"""
+  use Guardian, otp_app: :finance_fy
+
+  alias FinanceFy.User
+  alias FinanceFy.Repo
+
+  def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
+  def subject_for_token(_, _), do: {:error, :invalid_user}
+
+  def resource_from_claims(%{"sub" => user_id}) do
+    resource = Repo.get!(User, user_id)
+    {:ok, resource}
+  end
+  def resource_from_claims(_claims) do
+    {:error, :invalid_user}
+  end
+end
