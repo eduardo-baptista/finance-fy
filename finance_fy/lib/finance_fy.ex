a4b56df248270842230ae10bc4b6846f496a9af8
Eduardo Baptista <eduardo.baptista@vulcanet.com.br>
2021-03-21 17:45:35 -0300

    🚀 - Add authentication delegates

--- finance_fy/lib/finance_fy.ex
+++ finance_fy/lib/finance_fy.ex
@@ -6,4 +6,9 @@ defmodule FinanceFy do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias FinanceFy.User.{SignIn, SignUp}
+
+  defdelegate sign_in_user(email, password), to: SignIn, as: :call
+  defdelegate sign_up_user(params), to: SignUp, as: :call
 end
